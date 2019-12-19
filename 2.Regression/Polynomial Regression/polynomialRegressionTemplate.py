cc8a39eeb0143fd839133f8e8a23dc2d3e05ca33
Mayank Bajaj <41484766+mayank8200@users.noreply.github.com>
2019-12-19 18:01:07 +0530

    commit

---
+++ 2.Regression/Polynomial Regression/polynomialRegressionTemplate.py
@@ -0,0 +1,37 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, -1].values
+
+#Fitting the regressor Model
+#Create Regressor Here
+
+
+
+#predicting using polynomial regression
+regressor.predict([[1,6.5,6.5**2,6.5**3,6.5**4]])
+
+#Visualising the regression
+plt.scatter(X,y,color="Red")
+plt.plot(X,regressor.predict(X),color="Blue")
+plt.title("Truth or Bluff(Regression)")
+plt.xlabel("Position")
+plt.ylabel("Salary")
+plt.show()
+
+#Visualising the regression(for higher resolution and smoother curve)
+X_grid=np.arange(min(X),max(X),0.1)
+X_grid=X_grid.reshape((len(X_grid),1))
+plt.scatter(X,y,color="Red")
+plt.plot(X_grid,regressor.predict(X_grid),color="Blue")
+plt.title("Truth or Bluff(Regression)")
+plt.xlabel("Position")
+plt.ylabel("Salary")
+plt.show()
+
+
