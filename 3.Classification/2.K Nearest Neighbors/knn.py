f8750c21e46c7e0b97397eab228f2a0669e27438
Mayank Bajaj <41484766+mayank8200@users.noreply.github.com>
2020-03-20 11:23:53 +0530

    commit

--- 3.Classification/2.K Nearest Neighbors/knn.py
+++ 3.Classification/2.K Nearest Neighbors/knn.py
@@ -1,4 +1,4 @@
-# Classification template
+# K Nearest Neighbor
 
 # Importing the libraries
 import numpy as np
@@ -21,8 +21,9 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Fitting classifier to the Training set
-# Create your classifier here
-
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5,p = 2,metric = 'minkowski')
+classifier.fit(X_train,y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
@@ -42,7 +43,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Training set)')
+plt.title('KNN(Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -60,7 +61,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Test set)')
+plt.title('K-NN (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
