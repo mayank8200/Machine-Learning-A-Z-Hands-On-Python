81adc26a9d08032d6fb9dc51e7ef6e862ceb3c3d
Mayank Bajaj <41484766+mayank8200@users.noreply.github.com>
2020-03-26 18:50:18 +0530

    commit

---
+++ 4.Clustering/2.Hierarchical Clustering/hierarchical Clustering.py
@@ -0,0 +1,37 @@
+# Hierarchical Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:,[3,4]].values
+
+#Using Dendogram for optimal number of Clusters
+import scipy.cluster.hierarchy as sch
+dendogram = sch.dendrogram(sch.linkage(X,method='ward'))
+plt.title('Dendogram')
+plt.xlabel('Customers')
+plt.ylabel('Eucledian Distance')
+plt.show()
+
+
+#Apply hierarchical with optimal number of clusters
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters=5,affinity='euclidean',linkage='ward')
+y_hc = hc.fit_predict(X)
+
+
+# Visualising the clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Careful')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Standard')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Target')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Careless')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Sensible')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
