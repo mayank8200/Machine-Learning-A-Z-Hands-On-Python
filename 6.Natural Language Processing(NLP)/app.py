e19841fae771f3d432ddabff2c52710773de13b7
Mayank Bajaj <41484766+mayank8200@users.noreply.github.com>
2020-03-30 15:07:07 +0530

    commit

---
+++ 6.Natural Language Processing(NLP)/app.py
@@ -0,0 +1,48 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+cv = pickle.load(open('cv.pkl', 'rb'))
+corpus = pickle.load(open('corpus.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    review = [str(x) for x in request.form.values()]
+    review = review[0]
+    # Cleaning the texts
+    import re
+    import nltk
+    nltk.download('stopwords')
+    from nltk.corpus import stopwords
+    from nltk.stem.porter import PorterStemmer
+    review = re.sub('[^a-zA-Z]', ' ', review)
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+    X = cv.fit_transform(corpus).toarray()
+    output = model.predict([X[-1]])
+    if output[0] == 0:
+        text = "Negative Feedback"
+    else:
+        text = "Positivee Feedback"
+        
+    
+
+    return render_template('index.html', prediction_text=text)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- 6.Natural Language Processing(NLP)/natural_language_processing.py
+++ 6.Natural Language Processing(NLP)/natural_language_processing.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import pickle
 
 # Importing the dataset
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
@@ -31,7 +32,7 @@ X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
 # Fitting Naive Bayes to the Training set
@@ -45,3 +46,9 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+# save the model to disk
+filename = 'model.pkl'
+pickle.dump(classifier, open(filename, 'wb'))
+pickle.dump(cv, open('cv.pkl', 'wb'))
+pickle.dump(corpus, open('corpus.pkl', 'wb'))
