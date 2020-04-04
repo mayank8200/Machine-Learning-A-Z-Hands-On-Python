ff4747c03fc5c3e67c86132759202d303c175696
Mayank Bajaj <41484766+mayank8200@users.noreply.github.com>
2020-04-04 13:17:16 +0530

    commit

--- 8.Convolutional Neural Network/cnn.py
+++ 8.Convolutional Neural Network/cnn.py
@@ -67,3 +67,7 @@ classifier.fit_generator(training_set,
                          nb_epoch = 25,
                          validation_data = test_set,
                          nb_val_samples = 2000)
+
+
+
+
